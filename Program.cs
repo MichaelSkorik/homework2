e406dd797a8ac5d0191a59ff7ae7b0fd20362fe8
Michael <Michael@SKorik.com>
2023-07-08 11:20:37 +0300

    Ex_15

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿ 
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+Console.WriteLine("Enter day of week");
+int day = int.Parse(Console.ReadLine());
+if(day == 7 && day == 8 )
+{
+    Console.WriteLine(day + " -> Yes");
+}
+else
+{
+    Console.WriteLine(day + " -> No");
+}
