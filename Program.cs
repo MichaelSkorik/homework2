c1613f969fe52a82b8d83c72fffbcb938f33c3dc
Michael <Michael@SKorik.com>
2023-07-08 10:36:44 +0300

    Ex_10

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿ 
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.WriteLine("Enter three-digit number");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Second digit is " + (num / 10) % 10);
