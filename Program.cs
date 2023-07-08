ef64bf7a3aeb947e96f0c82c5ad31a5175a8a5ac
Michael <Michael@SKorik.com>
2023-07-08 10:56:21 +0300

    Ex_13

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
 ﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+Console.WriteLine("Enter number");
+string num = Console.ReadLine();
+if(int.Parse(num) % 1 < 100)
+{
+    Console.WriteLine("There is no third digit");
+}
+else
+{
+    Console.WriteLine("Third digit is " + num[2]);
+}
